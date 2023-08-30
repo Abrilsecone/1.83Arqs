f27b1a2c7110088fb956c89319eb5a992729da26
abril secone <abrilsecone@gmail.com>
2023-08-29 22:03:15 -0300

    Agregue otros proyctos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from './components/NavBar/NavBar';
 import HomePage from './components/HomePage/HomePage';
+import FeaturedProjects from './components/FeaturedProjectsPage/FeaturedProjectsPage';
 import ContactSection from './components/ContactPage/ContactPage';
 import Footer from './components/Footer/Footer';
 import OtherProjects from './components/OtherProjects/OtherProjects.jsx';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import AboutUs from './components/AboutUs/AboutUs'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 const App = () => {
@@ -12,28 +14,18 @@ const App = () => {
     <Router>
       <div className="landing-page">
         <NavBar />
-        <Routes> {/* Utilizar el componente 'Routes' como contenedor principal */}
-          {/* Ruta para la página de inicio */}
-          <Route path="/" element={<HomePage />} />
-
-          {/* Ruta para la página de otros proyectos */}
-          <Route path="/otros-proyectos" element={<OtherProjects />} />
-
-          {/* Ruta para agrupar featured, about-us y contact */}
-          <Route path="/featured" element={<FeatureAboutContact />} />
-          <Route path="/about-us" element={<FeatureAboutContact />} />
-          <Route path="/contact" element={<FeatureAboutContact />} />
-
-          {/* Ruta por defecto en caso de URL inválida */}
-          <Route path="*" element={<HomePage />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<FeatureAboutContact />} />
+        <Route path="/otros-proyectos" element={<OtherProjects />} />
+        <Route path="*" element={<HomePage />} />
+      </Routes>
         <Footer />
       </div>
     </Router>
   );
 };
 
-// Componente para agrupar las páginas featured, about-us y contact
+// Componente para agrupar las páginas featured, about-us, contact y homepage
 const FeatureAboutContact = () => (
   <>
     <div id="featured">
@@ -44,6 +36,9 @@ const FeatureAboutContact = () => (
     </div>
     <div id="contact">
       <ContactSection />
+    </div>
+    <div id="home">
+      <HomePage />
     </div>
   </>
 );
--- src/components/OtherProjects/OtherProjects.jsx
+++ src/components/OtherProjects/OtherProjects.jsx
@@ -1,8 +1,48 @@
-// OtherProjects.jsx
 import React from 'react';
+import styles from './OtherProjects.module.css';
+import ProjectCarousel from '../ProjectCarousel/ProjectCarousel';
+import image1 from '../../imagesHome/Ima1.png';
+import image2 from '../../imagesHome/Ima2.png';
+import image3 from '../../imagesHome/Ima3.png';
 
 const OtherProjects = () => {
-  // Contenido de la página Otros Proyectos
+  const projects = [
+    {
+      title: 'Casa Fran',
+      description: 'Vivienda construida bajo el programa Pro.Cre.Ar, que combina con maestría elementos tradicionales y contemporáneos. Esta residencia única se destaca por su exterior de ladrillo visto y paneles de chapa, creando una estética distintiva que mezcla lo rústico con lo moderno.',
+      images: [image1, image2, image3],
+    },
+    {
+      title: 'Proyecto B',
+      description: 'Descripción del Proyecto B.',
+      images: [image1, image2, image3],
+    },
+    {
+      title: 'Proyecto C',
+      description: 'Descripción del Proyecto C.',
+      images: [image1, image2, image3],
+    },
+    {
+      title: 'Proyecto D',
+      description: 'Descripción del Proyecto D.',
+      images: [image1, image2, image3],
+    },
+  ];
+
+  return (
+    <div className={styles.containerP}>
+      <h1 className={styles.titleP}></h1>
+      <div className={styles.projectListP}>
+        {projects.map((project, index) => (
+          <div key={index} className={styles.projectCardP}>
+            <ProjectCarousel images={project.images} />
+            <h2 className={styles.projectTitleP}>{project.title}</h2>
+            <p className={styles.projectDescriptionP}>{project.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default OtherProjects;
