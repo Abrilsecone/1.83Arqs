2a1874ba22351e926cf36d0b825702f05d58a84a
abril secone <abrilsecone@gmail.com>
2023-09-12 18:15:09 -0300

    home

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -3,10 +3,10 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/scrollbar';
 
-import myImage from '../../images/Ima1.png';
-import myImage1 from '../../imagesHome/Ima6.png';
-import myImage2 from '../../imagesHome/Ima2a.png';
-import myImage3 from '../../imagesHome/Ima4a.png';
+import myImage from '../../images/Ima1resp.png';
+import myImage1 from '../../imagesHome/ImaLand4.png';
+import myImage2 from '../../imagesHome/ImaLan2.png';
+import myImage3 from '../../imagesHome/ImaLan3.png';
 import myImage4 from '../../images/Ima1.png';
 
 import styles from './home.module.css';
@@ -27,17 +27,14 @@ const HomePage = () => {
         modules={[Scrollbar]}
         className="mySwiper"
       >
-        {/* Diapositiva 1: Imagen */}
         <SwiperSlide>
           <img src={myImage} alt="Mi Imagen" className={styles.homeImage} />
         </SwiperSlide>
 
-        {/* Diapositiva 2: Otro contenido */}
         <SwiperSlide>
           <img src={myImage1} alt="Mi Imagen" className={styles.homeImage} />
         </SwiperSlide>
 
-        {/* Diapositiva 3: Otro contenido */}
         <SwiperSlide>
           <img src={myImage2} alt="Mi Imagen" className={styles.homeImage} />
         </SwiperSlide>
@@ -45,7 +42,6 @@ const HomePage = () => {
         <SwiperSlide>
           <img src={myImage3} alt="Mi Imagen" className={styles.homeImage} />
         </SwiperSlide>
-        {/* Agrega más diapositivas según sea necesario */}
       </Swiper>
     </div>
   );
